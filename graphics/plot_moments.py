b22d22498cc4dd730d92d57b13de7c89258c87e4
Caio Brito <caio.vp.debrito@jyu.fi>
2026-05-11 11:05:45 +0200

    Change axes labels

--- graphics/plot_moments.py
+++ graphics/plot_moments.py
@@ -27,21 +27,21 @@ def main():
     cmap = plt.colormaps["coolwarm"]
     norm = plt.Normalize(vmin=n_values.min(), vmax=n_values.max())
 
-    fig, ax = plt.subplots(figsize=(7.0, 4.8))
+    fig, ax = plt.subplots()
 
-    for column, n in enumerate(n_values):
-        ax.plot(x, y[:, column], color=cmap(norm(n)), linewidth=1.8)
+    for column, n in zip(range(0, y.shape[1], 2), n_values[::2]):
+        ax.loglog(x, y[:, column], color=cmap(norm(n)), linewidth=1.8)
 
+    # Add colorbar
     sm = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
     sm.set_array([])
 
     cbar = fig.colorbar(sm, ax=ax)
     cbar.set_label(r"$n$")
 
-    ax.set_xlabel(r"$t/\tau_R$")
-    ax.set_ylabel(r"$|\varrho_{n,l}|/\varrho^{\mathrm{eq}}_{n,0}$")
+    ax.set_xlabel(r"$\tau/\tau_R$")
+    ax.set_ylabel(rf'$|\varrho_{{n,{args.l}}}|/\varrho^{{\mathrm{{eq}}}}_{{n,0}}$')
     ax.set_title(rf"$l = {args.l}$")
-    ax.grid(True, alpha=0.25)
 
     fig.tight_layout()
 
@@ -52,6 +52,9 @@ def main():
 
     plt.show()
 
+###
+##
+#
 
 if __name__ == "__main__":
     main()
